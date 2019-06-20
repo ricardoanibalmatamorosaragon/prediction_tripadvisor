8b5afdf74d4d88aef62bb47720e40bf92fa41327
ricardo_aragon <34106214+ricardoanibalmatamorosaragon@users.noreply.github.com>
2019-06-20 13:10:53 +0000

    demo in dash

---
+++ code/demo.py
@@ -0,0 +1,91 @@
+import dash
+from dash.dependencies import Input, Output
+import dash_table
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+
+df = pd.read_csv('/home/ricardo/Scrivania/prediction_tripadvisor/csv/overall_testing_sentimental_netbayesian.csv')
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    html.H1('RATING COMPLESSIVO'),
+    html.H3('risultati ottenuti con our net bayesian e sentimental analysis'),
+    dash_table.DataTable(
+        id='datatable-interactivity',
+        columns=[
+            {"name": i, "id": i, "deletable": True} for i in df.columns
+        ],
+        data=df.to_dict('records'),
+        editable=True,
+        filtering=True,
+        sorting=True,
+        sorting_type="multi",
+        row_selectable="multi",
+        row_deletable=True,
+        selected_rows=[],
+        pagination_mode="fe",
+        pagination_settings={
+            "current_page": 0,
+            "page_size": 10,
+        },
+    ),
+    html.Div(id='datatable-interactivity-container')
+])
+
+
+@app.callback(
+    Output('datatable-interactivity-container', "children"),
+    [Input('datatable-interactivity', "derived_virtual_data"),
+     Input('datatable-interactivity', "derived_virtual_selected_rows")])
+def update_graphs(rows, derived_virtual_selected_rows):
+    # When the table is first rendered, `derived_virtual_data` and
+    # `derived_virtual_selected_rows` will be `None`. This is due to an
+    # idiosyncracy in Dash (unsupplied properties are always None and Dash
+    # calls the dependent callbacks when the component is first rendered).
+    # So, if `rows` is `None`, then the component was just rendered
+    # and its value will be the same as the component's dataframe.
+    # Instead of setting `None` in here, you could also set
+    # `derived_virtual_data=df.to_rows('dict')` when you initialize
+    # the component.
+    if derived_virtual_selected_rows is None:
+        derived_virtual_selected_rows = []
+
+    dff = df if rows is None else pd.DataFrame(rows)
+
+    colors = ['#7FDBFF' if i in derived_virtual_selected_rows else '#0074D9'
+              for i in range(len(dff))]
+
+    return [
+        dcc.Graph(
+            id=column,
+            figure={
+                "data": [
+                    {
+                        "x": dff["hotel"],
+                        "y": dff["overall"],
+                        "type": "bar",
+                        "marker": {"color": colors},
+                    }
+                ],
+                "layout": {
+                    "xaxis": {"automargin": True},
+                    "yaxis": {
+                        "automargin": True,
+                        "title": {"text": "overall"}
+                    },
+                    "height": 250,
+                    "margin": {"t": 10, "l": 10, "r": 10},
+                },
+            },
+        )
+        # check if column exists - user may have deleted it
+        # If `column.deletable=False`, then you don't
+        # need to do this check.
+        for column in ["overall"] if column in dff
+    ]
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
